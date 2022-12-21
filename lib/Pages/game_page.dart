329237b49e034634e4f8e66981b456caec794a6e
Aditya Deshpande <54365703+aditya1331@users.noreply.github.com>
2022-12-21 12:56:40 +0530

    Printing the question on display

--- lib/Pages/game_page.dart
+++ lib/Pages/game_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frivia/Provider/Home_page_provider.dart';
 import 'package:provider/provider.dart';
+
 class HomePage extends StatelessWidget {
   double? _deviceHeight, _deviceWidth;
 
@@ -10,25 +11,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
-    return ChangeNotifierProvider(create: (_context)=>HomePageProvider(context: context)
-      ,child: _buildUI(),);
+    return ChangeNotifierProvider(
+      create: (_context) => HomePageProvider(context: context),
+      child: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
-    return Builder(
-      builder: (_context) {
-        _pageProvider = _context.watch<HomePageProvider>();
+    return Builder(builder: (_context) {
+      _pageProvider = _context.watch<HomePageProvider>();
+      if (_pageProvider!.questions != null) {
         return Scaffold(
           body: SafeArea(
             child: Container(
-              padding: EdgeInsets.symmetric(
-                  horizontal: _deviceHeight! * 0.05),
+              padding: EdgeInsets.symmetric(horizontal: _deviceHeight! * 0.05),
               child: _gameUI(),
             ),
           ),
         );
+      } else {
+        return const Center(
+          child: CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        );
       }
-    );
+    });
   }
 
   Widget _gameUI() {
@@ -36,51 +44,52 @@ class HomePage extends StatelessWidget {
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: [_questionText(),
+      children: [
+        _questionText(),
         Column(
           children: [
             _trueButton(),
-            SizedBox(height: _deviceHeight!*0.01,),
+            SizedBox(
+              height: _deviceHeight! * 0.01,
+            ),
             _flaseButton(),
           ],
         ),
-
       ],
     );
   }
 
   Widget _questionText() {
-    return const Text("Test Question 1 ghfdhkjdhk  hsjahkj",
-      style: TextStyle(
-          color: Colors.white, fontSize: 25, fontWeight: FontWeight.w400),);
+    return Text(
+      _pageProvider!.getCurrentQuestionText(),
+      style: const TextStyle(
+          color: Colors.white, fontSize: 25, fontWeight: FontWeight.w400),
+    );
   }
 
   Widget _trueButton() {
-    return MaterialButton(onPressed: () {},
+    return MaterialButton(
+      onPressed: () {},
       color: Colors.green,
       minWidth: _deviceWidth! * 0.8,
       height: _deviceHeight! * 0.1,
       child: const Text(
         "True",
-        style: TextStyle(color: Colors.white,
-            fontSize: 25),
+        style: TextStyle(color: Colors.white, fontSize: 25),
       ),
-
     );
   }
 
   Widget _flaseButton() {
-    return MaterialButton(onPressed: () {},
+    return MaterialButton(
+      onPressed: () {},
       color: Colors.red,
       minWidth: _deviceWidth! * 0.8,
       height: _deviceHeight! * 0.1,
       child: const Text(
         "False",
-        style: TextStyle(color: Colors.white,
-            fontSize: 25),
+        style: TextStyle(color: Colors.white, fontSize: 25),
       ),
-
     );
   }
-
 }
--- lib/Provider/Home_page_provider.dart
+++ lib/Provider/Home_page_provider.dart
@@ -4,28 +4,30 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 //the extension changeNotifier helps to keep track of the changes that occur within the class
-class HomePageProvider extends ChangeNotifier{
-    final Dio _dio =Dio(); //Client that pulls request
-    final int _maxQuestions  = 10;
-    BuildContext context;
+class HomePageProvider extends ChangeNotifier {
+  final Dio _dio = Dio(); //Client that pulls request
+  final int _maxQuestions = 10;
+  List? questions;
+  int _currentQuestion = 0;
+  BuildContext context;
 
-    HomePageProvider({required this.context}){
-      _dio.options.baseUrl = 'https://opentdb.com/api.php';
-      _getQuestionsfromAPI();
+  HomePageProvider({required this.context}) {
+    _dio.options.baseUrl = 'https://opentdb.com/api.php';
+    _getQuestionsfromAPI();
+  }
 
+  Future<void> _getQuestionsfromAPI() async {
+    var _response = await _dio.get('', queryParameters: {
+      'amount': 10,
+      'type': 'boolean',
+      'difficulty': 'easy'
+    });
+    var _data = jsonDecode(_response.toString());
+    questions = _data["results"];
+    notifyListeners(); //Indiacting chanfe has occured so that rebild is called
+  }
 
-    }
-
-    Future<void> _getQuestionsfromAPI() async{
-      var _response = await _dio.get(
-        '',
-        queryParameters: {
-          'amount':10,
-          'type':'boolean',
-          'difficulty': 'easy'
-        }
-      );
-      var _data = jsonDecode(_response.toString());
-      var questions = _data["results"]
-    }
+  String getCurrentQuestionText() {
+    return questions![_currentQuestion]["question"];
+  }
 }
