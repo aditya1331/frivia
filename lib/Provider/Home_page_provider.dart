8903ed18f7fbcba0b500bc4f8b5690bcd263143f
Aditya Deshpande <54365703+aditya1331@users.noreply.github.com>
2022-12-21 12:15:44 +0530

    Defining the path to request for given difficulty level print the request on console which is stored in _data variable

--- lib/Provider/Home_page_provider.dart
+++ lib/Provider/Home_page_provider.dart
@@ -1,15 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 //the extension changeNotifier helps to keep track of the changes that occur within the class
 class HomePageProvider extends ChangeNotifier{
     final Dio _dio =Dio(); //Client that pulls request
+    final int _maxQuestions  = 10;
     BuildContext context;
 
     HomePageProvider({required this.context}){
       _dio.options.baseUrl = 'https://opentdb.com/api.php';
-      print("hello");
+      _getQuestionsfromAPI();
+
+
     }
 
-
+    Future<void> _getQuestionsfromAPI() async{
+      var _response = await _dio.get(
+        '',
+        queryParameters: {
+          'amount':10,
+          'type':'boolean',
+          'difficulty': 'easy'
+        }
+      );
+      var _data = jsonDecode(_response.toString());
+      var questions = _data["results"]
+    }
 }
